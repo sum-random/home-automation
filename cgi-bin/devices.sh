de7e3047a8e81f5b9ae00830f8282adfe181edcc
Clayton Tucker <clayton.69.tucker@gmail.com>
2016-09-06 05:55:02 -0400

    M cgi-bin/devices.sh  M data/devices.php

--- cgi-bin/devices.sh
+++ cgi-bin/devices.sh
@@ -1,6 +1,11 @@
 #!/usr/local/bin/bash
 
-USERSSH="/usr/local/www/apache24/cgi-data/.ssh"
+APACHE="/usr/local/www/apache24"
+CGIDATA="$APACHE/cgi-data"
+
+USERSSH="$CGIDATA/.ssh"
+SSHAUTH="-i $USERSSH/id_dsa"
+SSHOPTIONS="$SSHAUTH -o StrictHostKeyChecking=no -o PasswordAuthentication=no"
 lname[1]="ctucker"
 lname[50]="ctucker"
 lname[64]="ctucker"
@@ -20,12 +25,11 @@ getRGB() {
 }
 
 readdevice() {
-	HN=$(grep "^$1\W" /etc/hosts | awk '{print $2}') 
-	CL=$(grep "^$1\W" /etc/hosts | awk '{print $3}') 
+	HN=$( nslookup $1 | grep name | sed -e 's/.*= //' -e 's/.claytontucker.*//' | tail -1)
 	STATS=$(if nc -zw 1 $1 2222 2>&1 | grep -qi succeeded ; then 
-		ST=$(ssh -q -l root -i /usr/local/www/apache24/cgi-data/.ssh/id_dsa -o StrictHostKeyChecking=no -p 2222 $1 'cat /sys/class/power_supply/battery/uevent' | grep 'STATUS' | cut -d = -f 2)
-		LD=$(ssh -q -l root -i /usr/local/www/apache24/cgi-data/.ssh/id_dsa -o StrictHostKeyChecking=no -p 2222 $1 'cat /proc/loadavg')
-		PCT=$(ssh -q -l root -i /usr/local/www/apache24/cgi-data/.ssh/id_dsa -o StrictHostKeyChecking=no -p 2222 $1 'cat /sys/class/power_supply/battery/uevent' | grep 'CAPACITY' | cut -d = -f 2)
+		ST=$(ssh -q -l root $SSHOPTIONS -p 2222 $1 'cat /sys/class/power_supply/battery/uevent' | grep 'STATUS' | cut -d = -f 2)
+		LD=$(ssh -q -l root $SSHOPTIONS -p 2222 $1 'cat /proc/loadavg')
+		PCT=$(ssh -q -l root $SSHOPTIONS -p 2222 $1 'cat /sys/class/power_supply/battery/uevent' | grep 'CAPACITY' | cut -d = -f 2)
 		RGB=$(getRGB $PCT)
 		printf "<TD>$ST $LD</TD><TD STYLE='background-color:#${RGB};'>$PCT</TD>"  
 	elif nc -zw 1 $1 22 2>&1 | grep -qi succeeded ; then
@@ -35,12 +39,12 @@ readdevice() {
 	        IDX=$(echo $1 | sed -e 's/[0-9]*\.[0-9]*\.[0-9]*\.\([0-9]*\)/\1/')
 		printf "<TD>"
 		if [ -n "$IDX" ] && [ -n "${lname[$IDX]}" ] ; then
-			PWRSTAT=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $USERSSH/id_dsa -n ${lname[$IDX]}@$IP "cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_STATUS | cut -d = -f 2" 2>/dev/null)
-			BATCAP=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $USERSSH/id_dsa -n ${lname[$IDX]}@$IP "cat /sys/class/power_supply/BAT0/capacity" 2>/dev/null)
+			PWRSTAT=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts $SSHOPTIONS -n ${lname[$IDX]}@$IP "cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_STATUS | cut -d = -f 2" 2>/dev/null)
+			BATCAP=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts $SSHOPTIONS -n ${lname[$IDX]}@$IP "cat /sys/class/power_supply/BAT0/capacity" 2>/dev/null)
 			[ -n "$BATCAP" ] && RGB=$(getRGB $BATCAP)
 			LOADAVG=""
-			LOADAVG=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $USERSSH/id_dsa -n ${lname[$IDX]}@$IP "cat /proc/loadavg" 2>/dev/null)
-			[ -z "$LOADAVG" ] && LOADAVG=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $USERSSH/id_dsa -n ${lname[$IDX]}@$IP "sysctl vm.loadavg" 2>/dev/null)
+			LOADAVG=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts $SSHOPTIONS -n ${lname[$IDX]}@$IP "cat /proc/loadavg" 2>/dev/null)
+			[ -z "$LOADAVG" ] && LOADAVG=$(ssh -o UserKnownHostsFile=$USERSSH/known_hosts $SSHOPTIONS -n ${lname[$IDX]}@$IP "sysctl vm.loadavg" 2>/dev/null)
 		fi
 		[ -n "$PWRSTAT" ] && printf "$PWRSTAT "
 		[ -n "$LOADAVG" ] && printf "$LOADAVG"
@@ -51,11 +55,11 @@ readdevice() {
 	else
 		printf "<TD>Down</TD>"
 	fi)
-	printf "<TR><TD>$CL</TD><TD>$HN</TD>$STATS</TR>\n";
+	printf "<TR><TD>$HN</TD>$STATS</TR>\n";
 }
 
-for IP in $(cat /etc/hosts | grep -vE '::1|127.0.0.1|10.4.69.[0-9]\W|^#' | awk '{print $1}'); do
-	readdevice $IP >>/usr/local/www/apache24/cgi-data/devices.new.txt 2>&1  &
+for IP in $( cat /etc/namedb/master/claytontucker.org | grep ^[A-Za-z] | grep 10.4.69 | awk '{print $4}' | sort | uniq); do
+	readdevice $IP >>$CGIDATA/devices.new.txt 2>&1  &
 done 
 wait
-mv -f /usr/local/www/apache24/cgi-data/devices.new.txt /usr/local/www/apache24/cgi-data/devices.txt
+mv -f $CGIDATA/devices.new.txt $CGIDATA/devices.txt
--- data/devices.php
+++ data/devices.php
@@ -1,6 +1,6 @@
-<TABLE><TR><TH>Class</TH><TH>Host</TH><TH>Status</TH><TH>Charge</TH></TR>
+<TABLE><TR><TH>Host</TH><TH>Status</TH><TH>Charge</TH></TR>
 <?php
 
-	system("cat ../cgi-data/devices.txt | sort | grep -av Down");
+	system("cat ../cgi-data/devices.txt | sort -f | grep -av Down");
 ?>
 </TABLE>
