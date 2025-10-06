84ad64ed9619546564537836b48bced32555502e
Clayton Tucker <clayton.69.tucker@gmail.com>
2025-10-06 12:48:44 -0400

    fix the mouseover on temperature graph

--- data/jscript/index.js
+++ data/jscript/index.js
@@ -61,6 +61,7 @@ function cpuTemperatureGraph() {
           .attr('stroke', function(d) {console.log(d[0] + ' ' + color(d[0]));return d3.color(color(d[0]));})
           .attr('fill', 'none')
           .attr('stroke-width', 3)
+          .on('mouseover',function(d){d3.select(this).raise();})
           .attr('id', function(d) {return d[0];})
           .attr('d', function(d) {return linefunc(d[1]);});
       base.append('g')
@@ -91,7 +92,7 @@ function cpuTemperatureGraph() {
           .style("font-size",function(d) {fs=22-d[0].length*1.5;return fs+"px";})
           .attr('x',5)
           .attr('y', function(temps){var t = temps[1]; return vscale(t[t.length-1].mavg);})
-          .on('mouseover',function(d){d.raise();})
+          .on('mouseover',function(d){d3.select(this).raise();})
           .attr('fill',function(d) {return d3.color(color(d[0])).brighter();})
           .attr('stroke',function(d) {return d3.color(color(d[0])).darker();})
           .attr('stroke-width','0.5');
