feb46fcefaef715cf01fd6eaac609a4e1f5d89f1
Clayton Tucker <clayton.69.tucker@gmail.com>
2026-02-09 11:11:01 -0500

    updates per pylint

--- wsgi-lib/weather.py
+++ wsgi-lib/weather.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 # system libraries
-import os,time,json
+import json
 import urllib.request
-import urllib.request,http.client
 from urllib.request import Request
-import requests
 from multiprocessing import Pool
-import datetime
 
 # local libraries here
 import db
+from config import config
 from logit import logit
 
     
@@ -40,15 +38,11 @@ def get_weather_html():
     weather = get_weather_list()
     for next_item in sorted(weather):
         if weather[next_item]['url'] == '/getforecast':
-            retval.append("<span><a href='{}' title='{}' target=_blank onMouseOver='forecastGraph();'><svg class='weatherthumb' id='FORECASTGRAPH'></SVG>"
-                          "</a></span>".format(weather[next_item]['url'],
-                                               weather[next_item]['ttl']))
+            retval.append(f"<span><a href='{weather[next_item]['url']}' title='{weather[next_item]['ttl']}' target=_blank onMouseOver='forecastGraph();'>"
+                          f"<svg class='weatherthumb' id='FORECASTGRAPH'></svg></a></span>")
         else:
-            retval.append("<span><a href='{}' title='{}' target=_blank><img class='weatherthumb' src='{}' alt='{}'>"
-                          "</a></span>".format(weather[next_item]['url'],
-                                               weather[next_item]['ttl'],
-                                               weather[next_item]['uri'],
-                                               weather[next_item]['alt']))
+            retval.append(f"<span><a href='{weather[next_item]['url']}' title='{weather[next_item]['ttl']}' target=_blank><img class='weatherthumb' "
+                          f"src='{weather[next_item]['uri']}' alt='{weather[next_item]['alt']}'></a></span>")
     retval.append('<script type="text/javascript">')
     retval.append('    setupWeather();')
     retval.append('</script>')
@@ -62,24 +56,24 @@ def get_weather_readings(request):
     mavg = 2 if not (pixels and end and start) else int((end-start)/300/pixels)+1
     readings=['host,timestamp,reading,mavg']
     try:
+        where_ts_bound= " WHERE timestamp % 300 = 0 AND "
+        where_ts_start=f" timestamp>={start}" if start else ""
+        where_ts_and =  " AND" if start and end else ""
+        where_ts_end = f" timestamp<={end}" if end else ""
+        where_host_and= " AND" if (start or end) and host else ""
+        where_host  =  f" host IN ('{host}')" if host else ""
         connection = db.open_sql_connection()
         cursor = connection.cursor()
-        query = "SELECT id, timestamp, host, reading, AVG(reading) " \
-         " OVER(PARTITION BY host ORDER BY timestamp ROWS BETWEEN {} PRECEDING AND {} FOLLOWING) AS moving_average " \
-         " FROM temperatures{}{}{}{}{}{}".format(mavg,mavg, \
-         " WHERE timestamp % 300 = 0 AND ", \
-         " timestamp>={}".format(start) if start else "", \
-         " AND" if start and end else "", \
-         " timestamp<={}".format(end) if end else "", \
-         " AND" if (start or end) and host else "", \
-         " host IN ('{}')".format(host) if host else "")
+        query = "SELECT host, timestamp, reading, AVG(reading) " \
+         f" OVER(PARTITION BY host ORDER BY timestamp ROWS BETWEEN {mavg} PRECEDING AND {mavg} FOLLOWING) AS moving_average " \
+         f" FROM temperatures{where_ts_bound}{where_ts_start}{where_ts_and}{where_ts_end}{where_host_and}{where_host}"
         if cursor.execute(query):
             for onereading in cursor.fetchall():
-                readings.append("{},{},{},{}".format(onereading[2],onereading[1],onereading[3],onereading[4]))
+                readings.append(f'{onereading[0]},{onereading[1]},{onereading[2]},{onereading[3]}')
         cursor.close()
         connection.close()
     except Exception as ex:
-        logit("{}".format(ex))
+        logit(f"{ex}")
     return readings
 
 # compare current average to average of all measurements
@@ -92,32 +86,32 @@ def get_current_vs_historical():
         #select b.alltime, a.current from (select 1 as one,avg(reading) as alltime from temperatures) b INNER JOIN (select 1 as one, avg(reading) as current from temperatures where timestamp=(select max(timestamp) from temperatures)) a ON a.one=b.one;"
         if cursor.execute(query):
             resultset = cursor.fetchone()
-            tempval = resultset[1] / resultset[0];
+            tempval = resultset[1] / resultset[0]
         cursor.close()
         connection.close()
     except Exception as ex:
-        logit("{}".format(ex))
+        logit(f"{ex}")
     redval = 255 if tempval>=1.25 else 0 if tempval <= 1 else (tempval-1)*256*4
     grnval = 255 if tempval<=1.25 else 0 if tempval >= 1.5 else (1.5-tempval)*256*4
     return [redval,grnval]
 
 def save_weather_reading(timestamp = None, host = None, reading = None):
     if timestamp is None or host is None or reading is None:
-        raise Exception("Cannot use these values - timestamp: {} host: {} reading: {}".format(timestamp, host, reading))
+        raise Exception(f"Cannot use these values - timestamp: {timestamp} host: {host} reading: {reading}")
     try:
         connection = db.open_sql_connection()
         cursor = connection.cursor()
-        query = "INSERT INTO temperatures(timestamp, host, reading) VALUES({}, '{}', {})".format(timestamp, host, reading)
+        query = f"INSERT INTO temperatures(timestamp, host, reading) VALUES({timestamp}, '{host}', {reading})"
         if cursor.execute(query):
             cursor.close()
             cursor=connection.cursor()
-            query = "SELECT * FROM temperatures WHERE timestamp={} AND host='{}'".format(timestamp,host)
+            query = f"SELECT * FROM temperatures WHERE timestamp={timestamp} AND host='{host}'"
             if cursor.execute(query):
                 return cursor.fetchone()
         cursor.close()
         connection.close()
     except Exception as ex:
-        logit("{}".format(ex))
+        logit(f"{ex}")
     # don't know what happened here, hope they can recover
     return None
 
@@ -130,42 +124,39 @@ def get_forecast(ip_address):
             (p1,p2,location) = k.split('_')
             for dline in v:
                 if dline['temperature'] is not None:
-                    thefinaldata.append("{},{},{}".format(location,dline['startTime'],dline['temperature']))
+                    thefinaldata.append(f"{location},{dline['startTime']},{dline['temperature']}")
     return thefinaldata
 
 def fetch_one_location(bundle):
     thefinaldata=""
     try:
-      useragent={'User-Agent':'household services host, clayton.69.tucker@gmail.com'}
-      # initial request to get the link to the data
-      apireq = Request('https://api.weather.gov/points/{}'.format(bundle[1]),headers=useragent)
-      req = urllib.request.urlopen(apireq)
-      theapidata = json.loads(req.read())
-      # now fetch the forecast data
-      uri=theapidata['properties']['forecastHourly']
-      apireq = Request(uri,headers=useragent)
-      req = urllib.request.urlopen(apireq)
-      theapidata = json.loads(req.read())
-      #db.set_val_for_key('one_raw_forecast',json.dumps(theapidata))
-      thefinaldata = json.dumps(theapidata['properties']['periods'])
-      (yr,mo,dy,tz) = theapidata['properties']['periods'][0]['startTime'].split('-')
-      db.set_val_for_key("forecast_data_{}".format(bundle[0]), thefinaldata, '{}-{}-{}'.format(yr,mo,dy))
+        useragent={'User-Agent':config()['misc']['user-agent']}
+        # initial request to get the link to the data
+        apireq = Request(f'https://api.weather.gov/points/{bundle[1]}',headers=useragent)
+        req = urllib.request.urlopen(apireq)
+        theapidata = json.loads(req.read())
+        # now fetch the forecast data
+        uri=theapidata['properties']['forecastHourly']
+        apireq = Request(uri,headers=useragent)
+        req = urllib.request.urlopen(apireq)
+        theapidata = json.loads(req.read())
+        #db.set_val_for_key('one_raw_forecast',json.dumps(theapidata))
+        thefinaldata = json.dumps(theapidata['properties']['periods'])
+        (yr,mo,dy,tz) = theapidata['properties']['periods'][0]['startTime'].split('-')
+        db.set_val_for_key(f'forecast_data_{bundle[0]}', thefinaldata, f'{yr}-{mo}-{dy}')
     except Exception as ex:
-      logit("{}".format(ex))
+        logit(f"{ex}")
     return thefinaldata
 
 
 if __name__ == '__main__':
-    theapiuris = [["BTR","30.5315,-91.1522" ],
-                  ["NUQ","37.4090,-122.0507"],
-                  ["TCS","32.1145,-110.9392"],
-                  ["DRT","29.6897,-101.1754"],
-                  ["RNO","39.4968,-119.7707"],
-                  ["SRQ","27.3070,-82.4951" ]]
-    db.set_val_for_key('forecast_links',json.dumps(theapiuris))
+    #theapiuris = [["BTR","30.5315,-91.1522" ],
+    #              ["NUQ","37.4090,-122.0507"],
+    #              ["TCS","32.1145,-110.9392"],
+    #              ["DRT","29.6897,-101.1754"],
+    #              ["RNO","39.4968,-119.7707"],
+    #              ["SRQ","27.3070,-82.4951" ]]
+    #db.set_val_for_key('forecast_links',json.dumps(theapiuris))
     theapiuris = json.loads(db.get_val_for_key('forecast_links'))
-    p = Pool(len(theapiuris))
-    retval = p.map(fetch_one_location, theapiuris)
-    p.close()
-    p.join()
-    #print("{}".format(get_forecast('10.4.69.64')))
+    with Pool(len(theapiuris)) as p:
+        catchval = p.map(fetch_one_location, theapiuris)
